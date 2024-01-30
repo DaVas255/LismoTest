18bc181c5a8627456d2f267508c9862d881a147e
Danil <DaVas255@gmail.com>
2024-01-30 22:08:59 +0600

    first commit

---
+++ src/components/header/Header.vue
@@ -0,0 +1,36 @@
+<script setup>
+import style from './Header.module.scss'
+</script>
+
+<template>
+  <header :class="style.header">
+    <div :class="style.header__logo">
+      <a href="#">
+        <img src="/src/assets/svg/tusur-logo.svg" alt="logo" />
+      </a>
+    </div>
+
+    <div :class="style.header__main">
+      <div :class="style.header__control">
+        <a href="#">
+          <img src="/src/assets/svg/notification.svg" alt="notification" />
+        </a>
+        <a href="#">
+          <img src="/src/assets/svg/chat.svg" alt="chat" />
+        </a>
+        <a href="#">
+          <img src="/src/assets/svg/calendar.svg" alt="calendar" />
+        </a>
+      </div>
+
+      <div :class="style.header__profile">
+        <div :class="style.header__user">
+          <img src="/src/assets/svg/profile-default-icon.svg" alt="profile-icon" />
+          <a href="#">Иван Иванов</a>
+        </div>
+
+        <button :class="style.header__exit">Выход</button>
+      </div>
+    </div>
+  </header>
+</template>
---
+++ src/components/main/Main.vue
@@ -0,0 +1,10 @@
+<script setup>
+import style from './Main.module.scss'
+</script>
+
+<template>
+  <div :class="style.main">
+    <aside :class="style.main__aside">ss</aside>
+    <main :class="style.main__content">as</main>
+  </div>
+</template>
